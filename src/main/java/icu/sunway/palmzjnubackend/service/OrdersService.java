09c87f3b171dccfcb338b8de14fb7b5f56769e57
gushen <w56038009@outlook.com>
2024-05-20 20:51:13 +0800

    🐛 fix: 修复了注解错误

--- src/main/java/icu/sunway/palmzjnubackend/service/OrdersService.java
+++ src/main/java/icu/sunway/palmzjnubackend/service/OrdersService.java
@@ -2,9 +2,11 @@ package icu.sunway.palmzjnubackend.service;
 
 import icu.sunway.palmzjnubackend.dao.OrdersDao;
 import icu.sunway.palmzjnubackend.pojo.OrdersPojo;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class OrdersService {
 
     private final OrdersDao ordersDao;
