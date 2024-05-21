e518b04deb73d38e63e8b1d8faf5b8e720694aab
gushen <w56038009@outlook.com>
2024-05-21 18:58:50 +0800

    ✨ feat: 添加订单接口

--- src/main/java/icu/sunway/palmzjnubackend/controller/OrdersController.java
+++ src/main/java/icu/sunway/palmzjnubackend/controller/OrdersController.java
@@ -1,7 +1,10 @@
 package icu.sunway.palmzjnubackend.controller;
 
 import icu.sunway.palmzjnubackend.pojo.OrdersPojo;
+import icu.sunway.palmzjnubackend.pojo.PostedOrderPojo;
+import icu.sunway.palmzjnubackend.pojo.Status;
 import icu.sunway.palmzjnubackend.service.OrdersService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,11 @@ public class OrdersController {
     public List<OrdersPojo> getAllOrders() {
         return ordersService.getAllOrders();
     }
+
+    @RequestMapping(value = "api/order", method = RequestMethod.POST)
+    public Status addOrder(@RequestBody PostedOrderPojo order) {
+        return ordersService.addOrder(order.getUser_id(), order.getTotal_price());
+    }
+
 
 }
--- src/main/java/icu/sunway/palmzjnubackend/service/OrdersService.java
+++ src/main/java/icu/sunway/palmzjnubackend/service/OrdersService.java
@@ -2,8 +2,11 @@ package icu.sunway.palmzjnubackend.service;
 
 import icu.sunway.palmzjnubackend.dao.OrdersDao;
 import icu.sunway.palmzjnubackend.pojo.OrdersPojo;
+import icu.sunway.palmzjnubackend.pojo.Status;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -17,6 +20,18 @@ public class OrdersService {
 
     public List<OrdersPojo> getAllOrders() {
         return ordersDao.selectList(null);
+    }
+
+    public Status addOrder(int userId, double totalPrice) {
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTime = time.format(formatter);
+        OrdersPojo ordersPojo = new OrdersPojo(null, userId, formattedTime, totalPrice, "下单成功");
+        if (ordersDao.insert(ordersPojo) > 0) {
+            return Status.SUCCESS;
+        } else {
+            return Status.ERROR;
+        }
     }
 
 }
