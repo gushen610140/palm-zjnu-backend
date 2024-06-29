00e79f27c81b11bada5ab29562aa82d1cb7c59e6
gushen <w56038009@outlook.com>
2024-06-29 15:14:04 +0800

    ✨ feat: 新增了圈子接口

---
+++ src/main/java/icu/sunway/palmzjnubackend/dao/MomentDao.java
@@ -0,0 +1,9 @@
+package icu.sunway.palmzjnubackend.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import icu.sunway.palmzjnubackend.pojo.MomentPojo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MomentDao extends BaseMapper<MomentPojo> {
+}
