9837a222b19c3206f748ac64a558c2a10f3d28db
gushen <w56038009@outlook.com>
2024-06-29 16:32:51 +0800

    ✨ feat: 优化了Pojo冗余的@TableField注解

--- src/main/java/icu/sunway/palmzjnubackend/pojo/CommentPojo.java
+++ src/main/java/icu/sunway/palmzjnubackend/pojo/CommentPojo.java
@@ -1,7 +1,6 @@
 package icu.sunway.palmzjnubackend.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,24 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommentPojo {
 
-    @TableId(value = "_id", type = IdType.ASSIGN_UUID)
+    @TableId(type = IdType.ASSIGN_UUID)
     private String id;
 
-    @TableField(value = "user_id")
     private String userId;
 
-    @TableField(value = "user_name")
     private String userName;
 
-    @TableField(value = "user_avatar")
     private String userAvatar;
 
-    @TableField(value = "date")
     private String date;
 
-    @TableField(value = "content")
     private String content;
 
-    @TableField(value = "likes")
     private Integer likes;
 }
--- src/main/java/icu/sunway/palmzjnubackend/pojo/MomentPojo.java
+++ src/main/java/icu/sunway/palmzjnubackend/pojo/MomentPojo.java
@@ -1,7 +1,6 @@
 package icu.sunway.palmzjnubackend.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -16,31 +15,23 @@ import java.util.List;
 @TableName(value = "moments")
 public class MomentPojo {
 
-    @TableId(value = "_id", type = IdType.ASSIGN_UUID)
+    @TableId(type = IdType.ASSIGN_UUID)
     private String id;
 
-    @TableField(value = "user_id")
     private String userId;
 
-    @TableField(value = "user_name")
     private String userName;
 
-    @TableField(value = "user_avatar")
     private String userAvatar;
 
-    @TableField(value = "date")
     private String date;
 
-    @TableField(value = "content")
     private String content;
 
-    @TableField(value = "images")
     private List<String> images;
 
-    @TableField(value = "likes")
     private Integer likes;
 
-    @TableField(value = "comments")
     private List<CommentPojo> comments;
 
 }
